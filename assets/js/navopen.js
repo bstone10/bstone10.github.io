0b9026d53af919eab5ed768bff064f8a259ea94d
bstone10 <40468168+bstone10@users.noreply.github.com>
2019-01-06 14:58:01 -0600

    first website upload yeet

---
+++ assets/js/navopen.js
@@ -0,0 +1,12 @@
+
+function openNav() {
+    document.getElementById("navBtn").classList.add("is-active");
+//    navBtn.classList.add("is-active");
+    document.getElementById("myNav").style.display = "block";
+
+}
+
+function closeNav() {
+
+  document.getElementById("myNav").style.display = "none";
+}
