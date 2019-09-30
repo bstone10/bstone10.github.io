f22445670c828cac1674fdc3fd607c87a7d426c4
Ben Stone <stone.ben.w@gmail.com>
2019-09-30 11:36:39 -0400

    Update question.js

--- jsQuiz/question.js
+++ jsQuiz/question.js
@@ -1,10 +1,89 @@
+
+
+
 class Question {
     constructor (text, choices, answer){
         this.text = text;
         this.choices = choices;
         this.answer = answer;
     }
+
+    isCorrectAnswer(choice){
+
+        return this.answer === choice;
+
+    }
 }
+
+class Quiz {
+    constructor(questions) {
+        this.score = 0;
+        this.questions = questions;
+        this.questionIndex = 0;
+    }
+
+    getQuestion() {
+
+        return this.questions[this.questionIndex];
+
+    }
+
+    guess(answer) {
+        if(this.getQuestion().isCorrectAnswer(answer)) {
+            
+            this.score++;
+            
+            }
+        
+        this.questionIndex++;
+    }
+
+    isEnded() {
+        return this.questionIndex === this.questions.length;
+    }   
+
+
+   
+}
+
+function guess(id, guess) {
+    var button = document.getElementById(id);
+    button.onclick = function() {
+        quiz.guess(guess);
+        populate();
+    }
+};
+
+function populate() {
+    if(quiz.isEnded()) {
+        showScores();
+    }
+    else {
+ 
+        var element = document.getElementById("question");
+        element.innerHTML = quiz.getQuestion().text;
+ 
+        var choices = quiz.getQuestion().choices;
+        for(var i = 0; i < choices.length; i++) {
+            var element = document.getElementById("choice" + i);
+            element.innerHTML = choices[i];
+            guess("btn" + i, choices[i]);
+        }
+ 
+    }
+};
+
+
+
+
+function showScores() {
+    var gameOverHTML = "<h1>Result</h1>";
+    gameOverHTML += "<h2 id='score'> Your scores: " + quiz.score + "</h2>";
+    var element = document.getElementById("quiz");
+    element.innerHTML = gameOverHTML;
+};
+
+
 
 var questions = [
     new Question("Hyper Text Markup Language Stand For?", ["JavaScript", "XHTML","CSS", "HTML"], "HTML"),
@@ -12,4 +91,5 @@ var questions = [
     new Question("What has not been covered in PUI lab?", ["HTML", "CSS","Javascript", "Java"], "Java"),
 ];
 
-new Question("Hyper Text Markup Language Stand For?", ["JavaScript", "XHTML","CSS", "HTML"], "HTML")
+var quiz = new Quiz(questions);
+populate();
